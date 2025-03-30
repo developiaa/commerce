45bfd83b465adc4d82094591689d3eadebbc60ef
장경재 <developiaa@gmail.com>
2025-03-30 12:21:04 +0900

    docs: readme 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,9 @@
-plugins {
-    java
-    id("org.springframework.boot") version "3.4.4"
-    id("io.spring.dependency-management") version "1.1.7"
-}
+subprojects {
+    repositories {
+        mavenCentral()
+    }
 
-group = "com.developia"
-version = "0.0.1-SNAPSHOT"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+    dependencies {
     }
 }
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
